728e516203c294e63736f922f5bd0c3f0482291a
i-lazarev <anion.lazarev@gmail.com>
2023-04-23 17:02:11 +0200

    project init wit springboot initializr added web dependency

---
+++ src/main/kotlin/ion/lazarev/springboot/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package ion.lazarev.springboot.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
